b839d8b374335743cdbf913fc6f0ff7952e61cd9
said <sexbythewind@gmail.com>
2025-12-19 12:00:57 +0800

    Initialize and working home display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import homeDisplay from "./template.js";
 import "./style.css";
 
+//initial display.
+homeDisplay();
+
+
 const homeBtn = document.querySelector("#home");
 
 homeBtn.addEventListener("click", homeDisplay);
--- src/template.js
+++ src/template.js
@@ -1,6 +1,8 @@
 export default function homeDisplay(){
     const contentId = document.querySelector("#content");
+    contentId.innerHTML = ``;
     const hDiv = document.createElement("div");
+    hDiv.classList.add("h1Div");
 
     const h1 = document.createElement("h1");
     h1.innerHTML = `WELCOME TO <br> SINAGTALA`;
