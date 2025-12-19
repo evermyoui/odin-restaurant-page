35d31e19c0a81833cddfd8c32beac62cc3ee514f
said <sexbythewind@gmail.com>
2025-12-19 14:13:12 +0800

    add menu tab

---
+++ src/menu.js
@@ -0,0 +1,18 @@
+export default function menuDisplay(){
+    const contentId = document.querySelector("#content");
+    const body = document.querySelector("body");
+    contentId.textContent = ``;
+    body.className = "";
+    
+    let index = 0;
+
+}
+
+class Menu {
+    constructor(h2, p, starCount){
+        this.h2 = h2;
+        this.p = p;
+        this.starCount = starCount;
+    }
+    
+}
