990ac3bf40048b3c8e850202389b6a7540a2f3c6
said <sexbythewind@gmail.com>
2025-12-19 14:13:53 +0800

    clean code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import homeDisplay from "./template.js";
+import menuDisplay from "./menu.js";
+
 import "./style.css";
+
 
 //initial display.
 homeDisplay();
 
-
 const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
 
 homeBtn.addEventListener("click", homeDisplay);
+menuBtn.addEventListener("click", menuDisplay);
--- src/template.js
+++ src/template.js
@@ -1,16 +1,28 @@
 export default function homeDisplay(){
     const contentId = document.querySelector("#content");
-    contentId.innerHTML = ``;
+    const body = document.querySelector("body");
+    contentId.className = "";
+    contentId.classList.add("home");
+
+    contentId.textContent = ``;
+    body.className = "";
+    body.classList.add("home");
+
     const hDiv = document.createElement("div");
     hDiv.classList.add("h1Div");
 
     const h1 = document.createElement("h1");
-    h1.innerHTML = `WELCOME TO <br> SINAGTALA`;
+    h1.textContent = `WELCOME TO`;
+    const br = document.createElement("br");
+    const h1Sinagtala = document.createElement("h1");
+    h1Sinagtala.textContent = "SINAGTALA";
 
     const p = document.createElement("p");
     p.textContent = `A restaurant dedicated to a beloved one`;
 
     hDiv.appendChild(h1);
+    hDiv.appendChild(br);
+    hDiv.appendChild(h1Sinagtala);
     hDiv.appendChild(p);
     contentId.appendChild(hDiv);
 }
