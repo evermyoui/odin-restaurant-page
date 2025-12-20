7b4117134cedd4b0c23234ee0b60ac56a92f79cf
said <sexbythewind@gmail.com>
2025-12-20 21:02:26 +0800

    Add new Menu, menu function, Add back and next button function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import homeDisplay from "./template.js";
-import menuDisplay from "./menu.js";
+import Menu from "./menu.js";
 
 import "./style.css";
 
@@ -9,6 +9,34 @@ homeDisplay();
 
 const homeBtn = document.querySelector("#home");
 const menuBtn = document.querySelector("#menu");
+const contentId = document.querySelector("#content");
+
+let currentMenu = 0;
+
+let menu = [
+    new Menu("SPAGHETTI", "Spaghetti is a classic Italian pasta made from durum wheat, water, and sometimes eggs, formed into long, thin, solid cylindrical strands"
+        , 5, 0
+    ),
+    new Menu("TUNA", "A versatile meal featuring flaky, firm fish, often canned and mixed with mayo, celery, and onion for salads or sandwiches",
+        4, 1
+    ),
+    new Menu("CROFFLE BERRY", "a hybrid pastry combining the flaky, buttery layers of a croissant with the crispy, grid-like texture of a waffle",
+        5, 2
+    )
+];
 
 homeBtn.addEventListener("click", homeDisplay);
-menuBtn.addEventListener("click", menuDisplay);
+menuBtn.addEventListener("click", () => {
+    menu[0].menuDisplay();
+});
+
+contentId.addEventListener("click", (e)=> {
+    if (e.target.classList.contains("nextBtn")){
+        currentMenu = (currentMenu + 1) % menu.length;
+        menu[currentMenu].menuDisplay();
+    }
+    if (e.target.classList.contains("backBtn")){
+        currentMenu = (currentMenu - 1) % menu.length;
+        menu[currentMenu].menuDisplay();
+    }
+});
--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,79 @@
-export default function menuDisplay(){
-    const contentId = document.querySelector("#content");
-    const body = document.querySelector("body");
-    contentId.textContent = ``;
-    body.className = "";
-    
-    let index = 0;
+export default class Menu {
+    constructor(menuTitle, description, starCount, index){
+        this.menuTitle = menuTitle;
+        this.description = description;
+        this.starCount = starCount;
+        this.index = index;
+    }
+    menuDisplay(){
+        const contentId = document.querySelector("#content");
+        const body = document.querySelector("body");
+        contentId.className = "";
+        contentId.textContent = "";
+        body.className = "";
 
+        body.classList.add(`index${this.index}`);
+
+        const menuDiv = document.createElement("div");
+        menuDiv.classList.add("menuDiv");
+
+        const backBtn = document.createElement("button");
+        backBtn.classList.add("backBtn");
+        backBtn.textContent = "BACK";
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const h2 = document.createElement("h2");
+        h2.textContent = `${this.menuTitle}`;
+
+        const p = document.createElement("p");
+        p.textContent = `${this.description}`;
+
+        const svgDiv = document.createElement("div");
+        svgDiv.classList.add("svgDiv");
+        this.svgRepeat(this.starCount, svgDiv);
+
+        const orderDiv = document.createElement("div");
+        orderDiv.classList.add("orderDiv");
+
+        const orderBtn = document.createElement("button");
+        orderBtn.classList.add("orderBtn");
+        orderBtn.textContent = `ORDER`;
+
+        const nextBtn = document.createElement("button");
+        nextBtn.classList.add("nextBtn");
+        nextBtn.textContent = `NEXT`;
+
+        menuDiv.appendChild(backBtn);
+        card.appendChild(h2);
+        card.appendChild(p);
+        card.appendChild(svgDiv);
+        card.appendChild(orderDiv);
+        menuDiv.appendChild(card);
+        orderDiv.appendChild(orderBtn);
+        menuDiv.appendChild(nextBtn);
+        contentId.appendChild(menuDiv);
+    }
+    svgRepeat(count, svgDiv) {
+    svgDiv.textContent = "";
+
+    for (let i = 0; i < count; i++) {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", "20");
+        svg.setAttribute("height", "20");
+        svg.setAttribute("viewBox", "0 0 100 100");
+
+        const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        polygon.setAttribute(
+            "points",
+            "50,5 61,39 98,39 68,59 79,93 50,72 21,93 32,59 2,39 39,39"
+        );
+        polygon.setAttribute("fill", "gold");
+
+        svg.appendChild(polygon);
+        svgDiv.appendChild(svg);
+    }
 }
 
-class Menu {
-    constructor(h2, p, starCount){
-        this.h2 = h2;
-        this.p = p;
-        this.starCount = starCount;
-    }
-    
 }
